27946f8585034bab8f9433e4ff25c3b7e8b808e5
Byungha <dev.byungha@gmail.com>
2020-11-30 22:57:00 +0900

    소스 정리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import CounterContainer from './containers/CounterContainer';
 import SampleContainer from './containers/SampleContainer';
 
 function App() {
   return (
     <div>
-      {/* <CounterContainer /> */}
       <SampleContainer />
     </div>
   );
